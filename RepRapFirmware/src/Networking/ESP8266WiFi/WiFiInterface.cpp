6fe7a7e684efddf6a088e0d854773d5e8ed61e43
henmabot <316876470+henmabot@users.noreply.github.com>
2026-09-19 11:50:34 +0000

    Revert "Poll WiFi state if status edge is missed"
    This reverts commit 537d21eb96a85651af4dc55e3d51b1258159d7f2.

--- RepRapFirmware/src/Networking/ESP8266WiFi/WiFiInterface.cpp
+++ RepRapFirmware/src/Networking/ESP8266WiFi/WiFiInterface.cpp
@@ -242,10 +242,6 @@ const uint32_t WiFiStableMillis = 500;					// Spin() fails in state starting2 wh
 const uint32_t WiFiStableMillis = 100;
 #endif
 
-#if defined(DA_VINCI_JR)
-const uint32_t WiFiModePollMillis = 250;
-#endif
-
 const unsigned int MaxHttpConnections = 4;
 
 #if SAME5x
@@ -958,7 +954,6 @@ void WiFiInterface::Spin() noexcept
 
 			if (rslt >= 0)
 			{
-				lastTickMillis = millis();
 				SetState(NetworkState::changingMode);
 			}
 			else
@@ -1032,16 +1027,9 @@ void WiFiInterface::Spin() noexcept
 
 	case NetworkState::changingMode:
 		// Here when we have asked the ESP to change mode. Don't leave this state until we have a new status report from the ESP.
-		if (   espStatusChanged && digitalRead(EspDataReadyPin)
-#if defined(DA_VINCI_JR)
-			|| millis() - lastTickMillis >= WiFiModePollMillis
-#endif
-		   )
+		if (espStatusChanged && digitalRead(EspDataReadyPin))
 		{
 			GetNewStatus();
-#if defined(DA_VINCI_JR)
-			lastTickMillis = millis();
-#endif
 			switch (currentMode)
 			{
 			case WiFiState::connecting:
